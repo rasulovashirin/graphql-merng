28ef1dbad96ecaec9a36c4cb616c25191949ada6
rasulovashirin <shirinrasulova1111@gmail.com>
2021-05-08 17:28:13 +0500

    add login with validation

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -3,17 +3,40 @@ const { UserInputError } = require("apollo-server")
 
 const { row } = require("../../database/postgres")
 const { SECRET_KEY } = require("../../config")
-const { validateRegisterInput } = require("../../util/validators")
+const { validateRegisterInput, validateLoginInput } = require("../../util/validators")
 
 module.exports = {
     Mutation: {
-        register : async(_, { registerInput: { username, email, password, confirmPassword } }, context, info) => {
-            // Validate user data
+        login: async(_, { username, password }) => {
+
+                const { errors, valid } = validateLoginInput(username, password)
+
+                if(!valid) {
+                    throw new UserInputError('Errors', { errors })
+                }
+
+                const user = await row(`select * from users where username = $1 AND user_password = crypt($2, user_password)`, username, password)
+
+                if(!user) {
+                    errors.general = 'User not found'
+                    throw new UserInputError('User not found', { errors })
+                }
+                user.token = null
+                const token = jwt.sign(user, SECRET_KEY)
+
+                user.token = token
+
+                return user
+
+        },
+        register: async(_, { registerInput: { username, email, password, confirmPassword } }, context, info) => {
+
             const { errors, valid } = validateRegisterInput(username, email, password, confirmPassword)
+
             if(!valid) {
                 throw new UserInputError('Errors', { errors })
             }
-            // Make sure user doesnt already exist
+
             const checkUsername = await row(`select * from users where username = $1`, username)
 
             if(checkUsername) {
@@ -23,8 +46,6 @@ module.exports = {
                     }
                 })
             }
-
-            // hash the password and auth token
 
             const user = await row(`insert into users(username, user_password, user_email) values ($1, crypt($2, gen_salt('bf')), $3) returning * `, username, password, email)
 
