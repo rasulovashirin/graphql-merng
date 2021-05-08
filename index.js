dcb3ee64bfe7240712e672d1aff0e71f582e66d4
rasulovashirin <shirinrasulova1111@gmail.com>
2021-05-08 13:27:22 +0500

    started server

---
+++ index.js
@@ -0,0 +1,22 @@
+const { ApolloServer, gql } = require("apollo-server")
+const { rows } = require("./database/postgres")
+
+
+const typeDefs = gql`
+    type Query {
+        sayHi: String!
+    }
+`
+
+const resolvers = {
+    Query: {
+        sayHi: () => "Hello World"
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+server.listen({port : 5000}).then(res => console.log(`Server running at ${res.url}`))
