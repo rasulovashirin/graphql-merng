fa96cb1fb2b880da13bf17e83afe03e99c0a4a86
rasulovashirin <shirinrasulova1111@gmail.com>
2021-05-17 13:15:09 +0500

    add delete post

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,3 +1,5 @@
+const { AuthenticationError } = require("apollo-server")
+
 const { rows, row } = require("../../database/postgres")
 const checkAuth = require("../../util/check-auth")
 
@@ -29,6 +31,22 @@ module.exports = {
             const user = checkAuth(context)
 
             return await row(`insert into posts(user_id, post_body) values ($1, $2) returning *`, user.user_id, body)
+        },
+        deletePost: async (_, { postId }, context) => {
+            const user = checkAuth(context)
+
+            try {
+                const post = await row(`select * from posts where post_id = $1`, postId)
+
+                if(user.user_id === post.user_id){
+                    return await row(`delete from posts where post_id = $1 returning *`, postId)
+                }
+                else {
+                    throw new AuthenticationError('Action not allowed ')
+                }
+            } catch (error) {
+                throw new Error(error)
+            }
         }
     }
 }
